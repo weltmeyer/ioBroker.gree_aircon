8cc5d53afaa3cac0c19681eb6eceaabfedbb1b1d
Jan Weltmeyer <j.weltmeyer@ad.weltmeyer.net>
2019-08-22 14:11:48 +0200

    some cleanup of template-variables not needed here.

--- main.js
+++ main.js
@@ -45,47 +45,10 @@ class GreeAircon extends utils.Adapter {
 		// this.config:
 		this.log.info('config ipAddress: ' + this.config.ipAddress);
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectAsync('testVariable', {
-			type: 'state',
-			common: {
-				name: 'testVariable',
-				type: 'boolean',
-				role: 'indicator',
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
-
 
 		// in this template all states changes inside the adapters namespace are subscribed
 		this.subscribeStates('*');
 
-		/*
-		setState examples
-		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync('testVariable', true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync('testVariable', { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync('admin', 'iobroker');
-		this.log.info('check user admin pw ioboker: ' + result);
-
-		result = await this.checkGroupAsync('admin', 'admin');
-		this.log.info('check group user admin group admin: ' + result);
 
 		this.Greeclient = new Gree.Client({ host: this.config.ipAddress });
 
